828078e6814d3a912bde9727881cbc813f0674de
Bhaskar Shukla <bhaskarshukla002@gmail.com>
2023-12-19 13:06:58 +0530

    Time: 1 ms (94.67%) | Memory: 44.3 MB (8.21%) - LeetSync

--- 46-permutations/permutations.java
+++ 46-permutations/permutations.java
@@ -1,21 +1,22 @@
 class Solution {
     public List<List<Integer>> permute(int[] nums) {
         List<Integer> num=new ArrayList<Integer>();
-        int len=nums.length;
+        // int len=nums.length;
         for(int i=0;i<nums.length;i++){
             num.add(nums[i]);
         }
         List<List<Integer>> ans=new ArrayList<>();
-        permute(num,ans,0,len);
+        permute(num,ans,0);
         return ans;
     }
-    public void permute(List<Integer> nums,List<List<Integer>> ans,int l,int h){
-        if(l==h)
+    public void permute(List<Integer> nums,List<List<Integer>> ans,int l){
+        if(l==nums.size()-1){
         ans.add(new ArrayList<>(nums));
+        return;}
         else
-        for(int i=l;i<h;i++){
+        for(int i=l;i<nums.size();i++){
             swap(nums,i,l);
-            permute(nums,ans,l+1,h);
+            permute(nums,ans,l+1);
             swap(nums,i,l);
         }
     }
