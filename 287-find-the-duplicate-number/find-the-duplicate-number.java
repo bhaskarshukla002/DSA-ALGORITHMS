957124c4ed418b12d64ba4b8bfb2ad1862398387
Bhaskar Shukla <bhaskarshukla002@gmail.com>
2023-12-27 21:31:49 +0530

    Time: 4 ms (85.70%) | Memory: 58.3 MB (15.88%) - LeetSync

--- 287-find-the-duplicate-number/find-the-duplicate-number.java
+++ 287-find-the-duplicate-number/find-the-duplicate-number.java
@@ -1,14 +1,17 @@
 class Solution {
     public int findDuplicate(int[] nums) {
-        HashSet<Integer> hs=new HashSet<>(); 
-        int index=0;
-        for(int i=0;i<nums.length;i++){
-            if(hs.contains(nums[index])){
-                return nums[index];
-            }
-            hs.add(nums[index]);
-            index=nums[index];
-        }
-        return -1;
+        int slow=0;
+        int fast=0;
+        do{
+            slow=nums[slow];
+            fast=nums[nums[fast]];
+        }while(slow!=fast);
+        slow=0;
+        do{
+            slow=nums[slow];
+            fast=nums[fast];
+        }while(slow!=fast);
+        return slow;
+
     }
 }
