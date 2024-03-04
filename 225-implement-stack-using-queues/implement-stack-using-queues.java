21949324fbacc38e796485b7f07361b043d49c8a
Bhaskar Shukla <bhaskarshukla002@gmail.com>
2024-03-05 00:16:48 +0530

    Time: 0 ms (100.00%) | Memory: 41.3 MB (51.42%) - LeetSync

--- 225-implement-stack-using-queues/implement-stack-using-queues.java
+++ 225-implement-stack-using-queues/implement-stack-using-queues.java
@@ -1,31 +1,56 @@
 class MyStack {
-    private final Queue<Integer> queue1=new LinkedList<Integer>();
-    private final Queue<Integer> queue2=new LinkedList<Integer>();
-    private int top;
+    // private final Queue<Integer> queue1=new LinkedList<Integer>();
+    // private final Queue<Integer> queue2=new LinkedList<Integer>();
+    // private int top;
+    // public MyStack() {
+        
+    // }
+    
+    // public void push(int x) {
+    //     queue1.offer(x);
+    //     top=x;
+    // }
+    
+    // public int pop() {
+    //     while(queue1.size()>1)
+    //     {
+    //         top=queue1.poll();
+    //         queue2.offer(top);
+    //     }
+    //     int val=queue1.poll();
+    //     while(!queue2.isEmpty()){
+    //         queue1.offer(queue2.poll());
+    //     }
+    //     return val;
+    // }
+    
+    // public int top() {
+    //     return top;
+    // }
+    
+    // public boolean empty() {
+    //     return queue1.isEmpty();
+    // }
+
+    private final Queue<Integer> queue1=new LinkedList<>();
     public MyStack() {
         
     }
     
     public void push(int x) {
         queue1.offer(x);
-        top=x;
+        // int i=queue1.size()-1;
+        for(int i=0;i<queue1.size()-1;i++){
+            queue1.offer(queue1.remove());
+        }
     }
     
     public int pop() {
-        while(queue1.size()>1)
-        {
-            top=queue1.poll();
-            queue2.offer(top);
-        }
-        int val=queue1.poll();
-        while(!queue2.isEmpty()){
-            queue1.offer(queue2.poll());
-        }
-        return val;
+        return queue1.remove();
     }
     
     public int top() {
-        return top;
+        return queue1.peek();
     }
     
     public boolean empty() {
