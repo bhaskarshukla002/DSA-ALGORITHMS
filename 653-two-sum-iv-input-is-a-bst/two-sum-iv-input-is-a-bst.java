0ea60319ada71ccfe651921b2baa5b287bca3a36
Bhaskar Shukla <bhaskarshukla002@gmail.com>
2024-07-01 18:25:21 +0530

    Time: 3 ms (65.97%) | Memory: 45 MB (46.14%) - LeetSync

--- 653-two-sum-iv-input-is-a-bst/two-sum-iv-input-is-a-bst.java
+++ 653-two-sum-iv-input-is-a-bst/two-sum-iv-input-is-a-bst.java
@@ -15,16 +15,64 @@
  */
 class Solution {
     public boolean findTarget(TreeNode root, int k) {
-        HashSet<Integer> hs=new HashSet<>();
-        return findTargetHelper(root,k,hs);
-    }
-    public boolean findTargetHelper(TreeNode root, int k,HashSet<Integer> hs) {
-        if(root==null)
-        return false;
-        if(hs.contains(k-root.val)){
-            return true;
+        // Sol 1:
+        // HashSet<Integer> hs=new HashSet<>();
+        // return findTargetHelper(root,k,hs);
+        // Sol 2:
+        BstIterator l=new BstIterator(root,false);
+        BstIterator r=new BstIterator(root,true);
+        int i=l.next();
+        int j=r.next();
+        while(i<j){
+            if(i+j==k){
+                return true;
+            }else if(i+j>k){
+                j=r.next();
+            }else{
+                i=l.next();
+            }
         }
-        hs.add(root.val);
-        return findTargetHelper(root.left,k,hs)||findTargetHelper(root.right,k,hs);
+        return false;
     }
+    // public boolean findTargetHelper(TreeNode root, int k,HashSet<Integer> hs) {
+    //     if(root==null)
+    //     return false;
+    //     if(hs.contains(k-root.val)){
+    //         return true;
+    //     }
+    //     hs.add(root.val);
+    //     return findTargetHelper(root.left,k,hs)||findTargetHelper(root.right,k,hs);
+    // }
+}
+public class BstIterator{
+    private Stack<TreeNode> st=new Stack<>();
+    boolean reverse=true;
+    public BstIterator(TreeNode root,boolean reverse){
+        // this.st=st;
+        this.reverse=reverse;
+        pushAll(root);
+    }
+
+    public boolean hasNext(){
+        return !st.isEmpty();
+    }
+
+    void pushAll(TreeNode root){
+        while(root!=null){
+            st.push(root);
+            if(reverse){
+                root=root.right;
+            }else{
+                root=root.left;
+            }
+        }
+    }
+
+    public int next(){
+        TreeNode temp=st.pop();
+        if(reverse==false) pushAll(temp.right);
+        else pushAll(temp.left);
+        return temp.val;
+    }
+
 }
