a67783226cccae9fd50c963a40344836722bad85
Bhaskar Shukla <bhaskarshukla002@gmail.com>
2024-07-14 19:09:36 +0530

    Time: 1 ms (43.82%) | Memory: 44.8 MB (38.58%) - LeetSync

--- 733-flood-fill/flood-fill.java
+++ 733-flood-fill/flood-fill.java
@@ -1,18 +1,30 @@
 class Solution {
-    static int[][] dirs={{-1,0},{0,1},{0,-1},{1,0}};
     public int[][] floodFill(int[][] image, int sr, int sc, int color) {
         int oldColor = image[sr][sc];
-        if(image[sr][sc]==color){
-            return image;
-        }
+        if(oldColor==color)
+        return image;
+        int m=image.length;
+        int n=image[0].length;
+        int[][] dirs={{-1,0},{0,1},{0,-1},{1,0}};
+        Queue<int[]> q=new LinkedList<>();
         image[sr][sc]=color;
-        for(int[] dir:dirs){
-            int r=sr+dir[0];
-            int c=sc+dir[1];
-            if(r>=0&&r<image.length&&c>=0&&c<image[0].length&& image[r][c] == oldColor)
-            floodFill(image,r,c,color);
+        q.add(new int[]{sr,sc});
+        while(!q.isEmpty()){
+            int size=q.size();
+            for(int i=0;i<size;i++){
+                int row=q.peek()[0];
+                int col=q.peek()[1];
+                q.remove();
+                for(int j=0;j<4;j++){
+                    int r=row+dirs[j][0];
+                    int c=col+dirs[j][1];
+                    if(r>=0&&r<m&&c>=0&&c<n&&image[r][c]==oldColor){
+                        image[r][c]=color;
+                        q.add(new int[]{r,c});
+                    }
+                }
+            }
         }
         return image;
-        
     }
 }
