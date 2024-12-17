a233decd94d4ef1f814ff3c85d8da2769adcf926
Bhaskar Shukla <bhaskarshukla002@gmail.com>
2024-12-17 19:46:08 +0530

    Time: 7 ms (64.16%) | Memory: 47.7 MB (76.61%) - LeetSync

--- 49-group-anagrams/group-anagrams.java
+++ 49-group-anagrams/group-anagrams.java
@@ -1,27 +1,21 @@
 class Solution {
     public List<List<String>> groupAnagrams(String[] strs) {
-        if(strs==null||strs.length==0)return new ArrayList<>();
-        int n=strs.length;
-        // List<List<String>> ans=new ArrayList<>();
-        char c[];
-        HashMap<String,List<String>> ans=new HashMap<>();
-        for(int i=0;i<n;i++){
-            String st=strs[i];
-            c=new char[26];
-            for(char ca:st.toCharArray()){
-                c[ca-'a']++;
+        List<List<String>> ans=new ArrayList<>(); 
+        HashMap<String,Integer> hm=new HashMap<String,Integer>();
+        
+        for(String s:strs){
+            char temp[] = s.toCharArray();
+            Arrays.sort(temp);
+            String sorted=new String(temp);
+            if(hm.containsKey(sorted)){
+                ans.get(hm.get(sorted)).add(s);
+            }else{
+                ans.add(new ArrayList<>());
+                ans.get(ans.size()-1).add(s);
+                hm.put(sorted,ans.size()-1);
             }
-            String str=String.valueOf(c);
-            if(!ans.containsKey(str)){
-                // ans.get(hs.get(str)).add(st);
-                ans.put(str,new ArrayList<>());
-            }
-            // List<String> temp=new ArrayList<>();
-            // temp.add(st);
-                ans.get(str).add(st);
-            // }
         }
-        return new ArrayList<>(ans.values());
+        return ans;
         
     }
 }
